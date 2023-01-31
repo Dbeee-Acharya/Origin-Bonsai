160177fa0540dbc07c26c71ebd8ece444db5b764
Dbeee-Acharya <107910547+Dbeee-Acharya@users.noreply.github.com>
2023-01-31 15:37:26 +0545

    remove external js and add internal js for feed page

--- 22067813-DibbeshworAcharya/webpages/JavaScript/feedbackPageScrpit.js
+++ 22067813-DibbeshworAcharya/webpages/JavaScript/feedbackPageScrpit.js
@@ -1,7 +1,9 @@
+//storing user input to a variable
 var nameCheck = document.getElementById('name');
 var emailCheck = document.getElementById('email');
 var commentCheck = document.getElementById('comment');
 
+//checking if any required feilds are empty and showing an alert accordingly
 function formValidate() {
     if(nameCheck.value == '' || emailCheck.value == '' || commentCheck.value == '' ) {
         alert("Field cannot be empty!")
@@ -10,6 +12,7 @@ function formValidate() {
     }
 }
 
+//function for meh rating, changes opacity of meh to 1 and of others to 0.5
 function meh() {
     if(document.querySelector("#mehRating").style.opacity != "1"){
         document.querySelector("#mehRating").style.opacity = "1";
@@ -20,6 +23,7 @@ function meh() {
     }
 }
 
+//function for smile rating, changes opacity of smile to 1 and of others to 0.5
 function smile() {
     if(document.querySelector("#smileRating").style.opacity != "1"){
         document.querySelector("#smileRating").style.opacity = "1";
@@ -31,6 +35,7 @@ function smile() {
 
 }
 
+//function for happy rating, changes opacity of happy to 1 and of others to 0.5
 function happy() {
     if(document.querySelector("#happyRating").style.opacity != "1"){
         document.querySelector("#happyRating").style.opacity = "1";
